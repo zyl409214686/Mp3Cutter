15757902f38c74e1b66eb806ad4c0b2f5541ee62
zouyulong <zouyulong@leho.com>
2016-11-18 12:27:59 +0800

    first commit

---
+++ app/src/main/java/mp3cutter/zyl/com/mp3cutter/activity/BaseActivity.java
@@ -0,0 +1,12 @@
+package mp3cutter.zyl.com.mp3cutter.activity;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by zouyulong on 16/10/15.
+ */
+
+public class BaseActivity extends FragmentActivity{
+    protected final String TAG = this.getClass().getSimpleName();
+
+}
---
+++ app/src/main/java/mp3cutter/zyl/com/mp3cutter/activity/MainActivity.java
@@ -0,0 +1,48 @@
+package mp3cutter.zyl.com.mp3cutter.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ImageView;
+
+import mp3cutter.zyl.com.mp3cutter.R;
+
+/**
+ * Created by zouyulong on 16/10/15.
+ */
+
+public class MainActivity extends BaseActivity implements View.OnClickListener {
+    ImageView mPlayIv, mForwardIv, mBackwardIv, mCutIv;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView(){
+        mPlayIv = (ImageView)findViewById(R.id.iv_play);
+        mForwardIv = (ImageView)findViewById(R.id.iv_forward);
+        mBackwardIv = (ImageView)findViewById(R.id.iv_backward);
+        mCutIv = (ImageView)findViewById(R.id.iv_cut);
+        mPlayIv.setOnClickListener(this);
+        mForwardIv.setOnClickListener(this);
+        mBackwardIv.setOnClickListener(this);
+        mCutIv.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int viewId = v.getId();
+        switch (viewId){
+            case R.id.iv_play:
+                break;
+            case R.id.iv_forward:
+                break;
+            case R.id.iv_backward:
+                break;
+            case R.id.iv_cut:
+                break;
+        }
+    }
+}
