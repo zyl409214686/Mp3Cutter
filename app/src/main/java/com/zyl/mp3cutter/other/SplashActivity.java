b97999fff1832b780278d8d25b98da27dd5e6a40
zouyulong <zouyulong@syswin.com>
2017-12-16 01:56:28 +0800

    cutter-1.0.1-欢迎页面增加lottie动画

--- app/src/main/java/com/zyl/mp3cutter/other/SplashActivity.java
+++ app/src/main/java/com/zyl/mp3cutter/other/SplashActivity.java
@@ -1,13 +1,15 @@
 package com.zyl.mp3cutter.other;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 import com.jaeger.library.StatusBarUtil;
+import com.orhanobut.logger.Logger;
 import com.zyl.mp3cutter.R;
 import com.zyl.mp3cutter.databinding.ActivitySplashBinding;
 
@@ -23,15 +25,48 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         StatusBarUtil.setColor(this, Color.TRANSPARENT);
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_splash);
-        new Handler().postDelayed(new Runnable() {
+        setListener();
+    }
+
+    private void setListener() {
+        ValueAnimator animator = new ValueAnimator().ofFloat(0f, 1f).setDuration(4000);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
-            public void run() {
-                Intent intent = new Intent();
-                intent.setClass(SplashActivity.this, MainActivity.class);
-                startActivity(intent);
-                SplashActivity.this.finish();
-                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+            public void onAnimationUpdate(ValueAnimator animation) {
+                mBinding.lottieImageview.setProgress(Float.parseFloat
+                        (animation.getAnimatedValue().toString()));
             }
-        }, 3000);
+        });
+        animator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                Logger.d("onAnimationStart");
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                Logger.d("onAnimationEnd");
+                goToMainActivity();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                Logger.d("onAnimationCancel");
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+                Logger.d("onAnimationRepeat");
+            }
+        });
+        animator.start();
+    }
+
+    private void goToMainActivity(){
+        Intent intent = new Intent();
+        intent.setClass(SplashActivity.this, MainActivity.class);
+        startActivity(intent);
+        SplashActivity.this.finish();
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
     }
 }
